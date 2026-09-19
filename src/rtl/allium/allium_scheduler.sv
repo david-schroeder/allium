aefd1e7191eb1d846149348d20868becb4336848
David Schröder <post@schroederdavid.de>
2026-09-19 23:23:40 +0200

    WIP scheduler: instruction decoding

--- src/rtl/allium/allium_scheduler.sv
+++ src/rtl/allium/allium_scheduler.sv
@@ -28,7 +28,11 @@ module allium_scheduler
 		CGRA configurations that the backend can use directly.
 	*/
 
-	/* Instruction decoding */
+	//////////////////////////
+	//                      //
+	// Instruction decoding //
+	//                      //
+	//////////////////////////
 
 	typedef enum logic [2:0] {
 		IMM, // LUI, AUIPC, JAL, JALR
@@ -40,7 +44,7 @@ module allium_scheduler
 			   // ADD, SUB, SLL, SLT, SLTU, XOR, SRL,
 			   // SRA, OR, AND
 		NOP // FENCE, ECALL, EBREAK, ARITH/LOAD with rd=x0
-	} instr_group_e;
+	} insn_group_e;
 
 
 	logic [ 6:0] opcode;
@@ -50,6 +54,20 @@ module allium_scheduler
 	logic [ 4:0] rs2;
 	logic [ 4:0] rd;
 	logic [11:0] imm12;
+	logic [11:0] imm_s;
+
+	insn_group_e insn_group;
+
+	logic [31:0] imm_data;
+
+	alu_op_e     alu_op;
+	logic        alu_is_imm;
+	logic [11:0] alu_imm;
+
+	mem_op_e     ldst_op;
+	logic [11:0] ldst_offset;
+
+	branch_e     branch_type;
 
 	assign opcode = bsd_insn_i[6:0];
 	assign funct3 = bsd_insn_i[14:12];
@@ -58,8 +76,76 @@ module allium_scheduler
 	assign rs2    = bsd_insn_i[24:20];
 	assign rd     = bsd_insn_i[11:7];
 	assign imm12  = bsd_insn_i[31:20];
+	assign imm_s  = {bsd_insn_i[31:25], bsd_insn_i[11:7]};
 
-	/* Configuration management logic */
+	assign ldst_offset = insn_group == STORE ? imm_s : imm12;
+
+	always_comb begin
+		casez (opcode)
+			7'b0?10111,
+			7'b110?111: insn_group = IMM;
+			7'b1100011: insn_group = BRANCH;
+			7'b0000011: insn_group = LOAD;
+			7'b0100011: insn_group = STORE;
+			7'b0?10011: insn_group = ARITH;
+			default   : insn_group = NOP;
+		endcase
+
+		if (insn_group inside {IMM, LOAD, ARITH}) begin
+			if (rd == 5'b0) insn_group = NOP;
+		end
+
+		case (funct3)
+			3'b000: alu_op = bsd_insn_i[30] && !opcode[5] ? SUB : ADD;
+			3'b001: alu_op = SLL;
+			3'b010: alu_op = SLT;
+			3'b011: alu_op = SLTU;
+			3'b100: alu_op = XOR;
+			3'b101: alu_op = bsd_insn_i[30] ? SRA : SRL;
+			3'b110: alu_op = OR;
+			3'b111: alu_op = AND;
+			default: alu_op = AND;
+		endcase
+
+		case (opcode)
+			// LUI
+			7'b0110111: imm_data = {bsd_insn_i[31:12], 12'b0};
+			// AUIPC
+			7'b0010111: imm_data = {bsd_insn_i[31:12], 12'b0} + bsd_pc_i;
+			// JAL, JALR
+			7'b1101111,
+			7'b1100111: imm_data = bsd_pc_i + 4;
+			default   : imm_data = '0;
+		endcase
+
+		case (funct3)
+			3'b000: ldst_op = insn_group == STORE ? SB : LB;
+			3'b001: ldst_op = insn_group == STORE ? SH : LH;
+			3'b010: ldst_op = insn_group == STORE ? SW : LW;
+			3'b100: ldst_op = LBU;
+			3'b101: ldst_op = LHU;
+			default: ldst_op = LBU;
+		endcase
+
+		case (funct3)
+			3'b000: branch_type = BEQ;
+			3'b001: branch_type = BNE;
+			3'b100: branch_type = BLT;
+			3'b101: branch_type = BGE;
+			3'b110: branch_type = BLTU;
+			3'b111: branch_type = BGEU;
+			default: branch_type = BEQ;
+		endcase
+	end
+
+	assign alu_is_imm = !opcode[5];
+	assign alu_imm    = imm12;
+
+	////////////////////////////////////
+	//                                //
+	// Configuration management logic //
+	//                                //
+	////////////////////////////////////
 
 	cgra_cfg_t cfg_d, cfg_q;
 	assign config_o = cfg_q;
